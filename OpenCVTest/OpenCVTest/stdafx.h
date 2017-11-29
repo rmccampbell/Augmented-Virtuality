702698ee819264d7bdda6fba8a4bd40e7fbd7c63
rmccampbell <rmccampbell7@gmail.com>
2017-11-29 18:50:02 -0500

    re-added with gitignores

---
+++ OpenCVTest/OpenCVTest/stdafx.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <stdint.h>
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/videoio.hpp"
---
+++ OpenCVTestUnity/Assets/OpenCVTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class OpenCVTest : MonoBehaviour {
+
+    [DllImport("OpenCVTest", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void Initialize();
+
+    [DllImport("OpenCVTest", CallingConvention = CallingConvention.Cdecl)]
+    public static extern byte[] ProcessFrame();
+
+    Texture2D tex;
+
+    private void Start()
+    {
+        Initialize();
+        tex = new Texture2D(640, 480, TextureFormat.BGRA32, false);
+        GetComponent<Renderer>().material.mainTexture = tex;
+    }
+
+    private void Update()
+    {
+        byte[] imgData = ProcessFrame();
+        tex.LoadRawTextureData(imgData);
+        tex.Apply();
+    }
+}
